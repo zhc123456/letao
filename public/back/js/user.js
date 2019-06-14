5972414881385c79b406ed6f04337b46ffa49f84
zhc123456 <530699367@qq.com>
2019-06-14 18:00:26 +0800

    用户管理分页渲染完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,21 +1,47 @@
 
 $(function(){
-  $.ajax({
-    type: "get",
-    url: "/user/queryUser",
-    data: {
-      page: 1,
-      pageSize: 5
-    },
-    dataType: "json",
-    success: function( info ){
-      console.log(info);
-     var htmlStr = template('tpl',info);
-     $('tbody').html(htmlStr);
-    }
+ 
+  var currentPage = 1;
+  var pasgeSize = 5;
+
+  render();
+
+  function render(){
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize: pasgeSize,
+      },
+      dataType: "json",
+      success: function( info ){
+        console.log(info);
+       var htmlStr = template('tpl',info);
+       $('tbody').html(htmlStr);
+      }
+    });
+  
+    $('#paginator').bootstrapPaginator({
+      //配置bootstrap 版本
+      bootstrapMajorVersion: 3,
+      // 指定总页数
+      totalPage: Math.ceil( info.total / info.size),
+      //当前页
+      currentPage: info.page,
+
+      onPageClicked: function(a,b,c,page){
+
+        currentPage = page;
+        render();
 
 
-  })
+      }
+
+    });
+  
+  }
+
 
 
 });
